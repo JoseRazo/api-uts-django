5c54a61d4faa981b8cbb834fe81900e6bd6faea5
Jose Razo <jrazo@utsalamanca.edu.mx>
2023-03-17 13:59:05 -0600

    cambios en readme

--- api_uts/urls.py
+++ api_uts/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.conf.urls.static import static
+# from django.urls import re_path
+# from django.views.static import serve
 from django.conf import settings
 from django.views.generic.base import RedirectView
 
@@ -25,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('noticias.urls')),
     path('api/', include('becas.urls')),
+    #re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
