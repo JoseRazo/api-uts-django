dc29f52cd53e42148423de9fc276b9c1b84e55c8
Jose Razo <joserrp.13@gmail.com>
2023-05-25 09:24:30 -0600

    condicion para cambiar de http a https de imagenes

--- noticias/serializers.py
+++ noticias/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 from .models import Articulo, ArticuloImagen
 
@@ -9,6 +10,34 @@ class ArticuloImagenSerializer(serializers.ModelSerializer):
 
 class ArticuloSerializer(serializers.ModelSerializer):
     imagenes = ArticuloImagenSerializer(many=True)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # Obtén la URL de la imagen del objeto actual
+        imagen_url = representation['imagen']
+
+        # Verifica si la URL comienza con "http://" y reemplázala por "https://"
+        if imagen_url.startswith('http://') and not settings.DEBUG:
+            imagen_url = imagen_url.replace('http://', 'https://')
+
+        # Actualiza la URL de la imagen en la representación
+        representation['imagen'] = imagen_url
+
+        # Verifica si hay imágenes adicionales
+        if 'imagenes' in representation and not settings.DEBUG:
+            for imagen_data in representation['imagenes']:
+                imagen_url = imagen_data['imagen']
+
+                # Verifica si la URL comienza con "http://" y reemplázala por "https://"
+                if imagen_url.startswith('http://'):
+                    imagen_url = imagen_url.replace('http://', 'https://')
+
+                # Actualiza la URL de la imagen en la representación de cada objeto ArticuloImagen
+                imagen_data['imagen'] = imagen_url
+
+        return representation
+
     class Meta:
         model = Articulo
         # fields = '__all__'
