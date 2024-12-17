aca95ae1e2dbeb37091929d49aa8b7b58b893e92
JoseRazo <joserrp.13@gmail.com>
2024-12-17 14:16:18 -0600

    cambiar http por https en produccion para las urls

--- becas/serializers.py
+++ becas/serializers.py
@@ -1,13 +1,33 @@
+from django.conf import settings
 from rest_framework import serializers
 from .models import Institucion, Beca, Convocatoria
 
+def update_archivo_url(archivo_url):
+    # Verifica si la URL de la imagen comienza con "http://" y reemplázala por "https://"
+    if archivo_url and archivo_url.startswith('http://') and not settings.DEBUG:
+        updated_url = archivo_url.replace('http://', 'https://')
+        return updated_url
+
+    return archivo_url
+
 class ConvocatoriaSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        # Obtiene la representación del objeto
+        representation = super().to_representation(instance)
+        
+        # Actualiza las URLs de los archivos
+        representation['archivo_convocatoria'] = update_archivo_url(representation['archivo_convocatoria'])
+        representation['archivo_resultado'] = update_archivo_url(representation['archivo_resultado'])
+        
+        return representation
+    
     class Meta:
         model = Convocatoria
         fields = '__all__'
 
 class BecaSerializer(serializers.ModelSerializer):
     convocatorias = ConvocatoriaSerializer(many=True)
+    
     class Meta:
         model = Beca
         # fields = '__all__'
@@ -15,6 +35,7 @@ class BecaSerializer(serializers.ModelSerializer):
 
 class InstitucionSerializer(serializers.ModelSerializer):
     becas = BecaSerializer(many=True)
+    
     class Meta:
         model = Institucion
         # fields = '__all__'
--- bolsa_trabajo_uts/serializers.py
+++ bolsa_trabajo_uts/serializers.py
@@ -1,7 +1,21 @@
+from django.conf import settings
 from rest_framework import serializers
 from .models import Vacante, Categoria, Descripcion, FormatoReferencia
 
+def update_archivo_url(archivo_url):
+    # Verifica si la URL de la imagen comienza con "http://" y reemplázala por "https://"
+    if archivo_url and archivo_url.startswith('http://') and not settings.DEBUG:
+        updated_url = archivo_url.replace('http://', 'https://')
+        return updated_url
+
+    return archivo_url
+
 class FormatoReferenciaSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['archivo'] = update_archivo_url(representation['archivo'])
+        return representation
+    
     class Meta:
         model = FormatoReferencia
         fields = '__all__'
@@ -20,7 +34,17 @@ class VacanteSerializer(serializers.ModelSerializer):
     categoria = CategoriaSerializer()
     descripcion = DescripcionSerializer()
     formato_referencia = FormatoReferenciaSerializer()
-
+    
+    def to_representation(self, instance):
+        # Obtiene la representación del objeto
+        representation = super().to_representation(instance)
+        
+        # Actualiza las URLs de los archivos
+        representation['convocatoria'] = update_archivo_url(representation['convocatoria'])
+        representation['resultados'] = update_archivo_url(representation['resultados'])
+        
+        return representation
+    
     class Meta:
         model = Vacante
         fields = '__all__'
